1d2e7357556f1f7352a2659e8a179b4108be0380
Aaron Langley <alangley345@gmail.com>
2020-01-03 13:24:23 -0500

    Added more pieces

--- bastionHost.tf
+++ bastionHost.tf
@@ -1,3 +1,4 @@
+#creates VPC
 resource "aws_vpc" "Edge" {
   cidr_block = "10.10.10.0/24"
   instance_tenancy = "dedicated"
@@ -6,3 +7,40 @@ resource "aws_vpc" "Edge" {
       Name = "Edge"
   }
 }
+
+#Subnet for edge devices
+resource "aws_subnet" "Edge" {
+  vpc_id     = "${aws_vpc.Edge}"
+  cidr_block = "10.10.10.0/24"
+
+  tags = {
+    Name = "Edge"
+  }
+}
+
+#Internet gateway for Edge VPC
+resource "aws_internet_gateway" "Edge" {
+  vpc_id = "${aws_vpc.Edge}"
+
+  tags = {
+    Name = "Edge"
+  }
+}
+
+#create EIP resource "aws_eip" "bar" {
+  vpc = true
+  instance                  = "${aws_instance.foo.id}"
+  associate_with_private_ip = "10.0.0.12"
+  depends_on                = ["aws_internet_gateway.gw"]
+}
+
+#creates NAT gateway
+
+resource "aws_eip" "Edge" {
+  vpc = true
+
+  instance                  = "${aws_instance.foo.id}"
+  associate_with_private_ip = "10.0.0.12"
+  depends_on                = ["aws_internet_gateway.gw"]
+}
+
