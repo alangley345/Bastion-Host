7e04b0dc0ae372ea2db6ebb009c35e71de873730
Aaron Langley <alangley345@gmail.com>
2020-01-03 15:42:00 -0500

    Added nat gateway and EIP allocation

--- bastionHost.tf
+++ bastionHost.tf
@@ -6,18 +6,18 @@ provider "aws" {
 
 #creates VPC
 resource "aws_vpc" "Edge" {
-  cidr_block = "10.10.10.0/24"
+  cidr_block       = "10.10.10.0/24"
   instance_tenancy = "dedicated"
 
   tags = {
-      Name = "Edge"
+    Name = "Edge"
   }
 }
 
 #Internet gateway for Edge VPC
 resource "aws_internet_gateway" "Edge" {
-  vpc_id = "${aws_vpc.Edge.id}"
-  depends_on = ["aws_vpc.Edge"]
+  vpc_id     = "${aws_vpc.Edge.id}"
+  depends_on = [aws_vpc.Edge]
 
   tags = {
     Name = "Edge"
@@ -28,10 +28,25 @@ resource "aws_internet_gateway" "Edge" {
 resource "aws_subnet" "Edge" {
   vpc_id     = "${aws_vpc.Edge.id}"
   cidr_block = "10.10.10.0/24"
-  depends_on = ["aws_vpc.Edge"]
- 
+  depends_on = [aws_vpc.Edge]
+
   tags = {
     Name = "Edge"
   }
 }
 
+#creating aws_eip
+resource "aws_eip" "edge-1" {
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+#creates NAT gateway
+
+resource "aws_nat_gateway" "Edge" {
+  allocation_id             ="${aws_eip.edge-1.id}"
+  depends_on                = [aws_internet_gateway.Edge]
+  subnet_id                 = "${aws_subnet.Edge.id}"
+  private_ip                = "10.10.10.1"
+}
